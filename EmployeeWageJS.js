7dcca77411c888524e3d8b060b8441cc81554676
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-08 21:44:32 +0530

    [sasic] Add . Calculating Wages For a Month

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -14,6 +14,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
@@ -30,3 +31,11 @@ function getWorkingHours(empCheck) {
 empHrs=getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage is : " + empWage);
+
+// UC4 Calculating Wages for a Month 
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + empHrs + " Employee Wage : " + empWage);
