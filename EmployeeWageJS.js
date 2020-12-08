f494d24b9cc14b491fc163974007d6d55dcbd655
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-08 21:35:33 +0530

    [sasic] Refactor . To Write a Function to get Working Hours

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -7,7 +7,8 @@ if (empCheck == IS_ABSENT) {
     console.log("employee is Present");
 }
 
-// UC2 Calculating Daily Employee Wage
+// UC => 2 Calculating Daily Employee Wage
+// UC => 3 Refactor to Write in Function
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -16,15 +17,16 @@ const WAGE_PER_HOUR = 20;
 
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+empHrs=getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage is : " + empWage);
